47252eeb8aa0047e6f8130c04d73f63e6a5e001c
Jill van Put <52222203+jillvp@users.noreply.github.com>
2019-10-22 07:20:42 +0200

    Exercise 9 - For loop with step.py

---
+++ 6 - Program Flow Control in Python/Exercise 9 - For loop with step.py
@@ -0,0 +1,8 @@
+# Coding Exercise 9 - For loop with step
+
+# Write a program to print out all the numbers from 0 to 100 that are divisble by 7.
+
+# Note that zero is considered to be divisble by all other integers, so your output should include zero.
+
+for i in range(0, 100, 7):
+    print('{}'.format(i))
